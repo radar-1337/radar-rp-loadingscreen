49fb66f880303f5f7e315249e67ba4f08d2788e9
radar-1337 <47488340+radar-1337@users.noreply.github.com>
2019-03-07 18:55:33 +0100

    Add files via upload

---
+++ config.js
@@ -0,0 +1,15 @@
+/* Need Help? Join my discord @ discord.gg/yWddFpQ */
+var config = {
+    /* In milliseconds */
+    "transitionInterval": 1250,
+    "imgInterval": 8000,
+    "music": true,
+    /* Insert the Youtube Video ID below */
+    "videoID": "RZeId0o4isE",
+    "text": {
+        "title": "BananBåten Roleplay",
+        "link": "discord.gg/y6wZbCa",
+    },
+    /* You dont have to include the '/imgs/' dir AND ALSO REMEMBER TO ADD THESE IMGS IN YOUR __resource.lua*/ 
+    "images": ['bg1.png', 'bg2.png', 'bg3.png', 'bg4.png', 'bg5.png']
+}
